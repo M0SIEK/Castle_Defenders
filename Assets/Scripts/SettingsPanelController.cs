c8326d825ebba224749be33af7e53d5bfb93d53a
Filip <127895941+Filip002@users.noreply.github.com>
2024-11-16 18:24:21 +0100

    update GameSettingsScene and SettingsPanelController.cs

--- Assets/Scripts/SettingsPanelController.cs
+++ Assets/Scripts/SettingsPanelController.cs
@@ -3,13 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements.Experimental;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SettingsPanelController : MonoBehaviour
 {
     public GameObject settingsPanel;
+    public Toggle SoundToggle;
+    public Toggle MusicToggle;
+
+    private bool soundOn;
+    private bool musicOn;
     private void Start()
     {
         settingsPanel.SetActive(false);
+        soundOn = PlayerPrefs.GetInt("sound") == 0 ? false : true;
+        musicOn = PlayerPrefs.GetInt("music") == 0 ? false : true;
+        
+        if(soundOn != SoundToggle)
+        {
+            SoundToggle.isOn = soundOn;
+        }
+        if(musicOn != MusicToggle)
+        {
+            MusicToggle.isOn = musicOn;
+        }
     }
     public void ShowSettingsPanel()
     {
@@ -32,6 +49,18 @@ public class SettingsPanelController : MonoBehaviour
     {
         Debug.Log("Load " + sceneName);
         SceneManager.LoadScene(sceneName);   
+    }
+
+    public void OnSoundToggle()
+    {
+        soundOn = SoundToggle.isOn;
+        PlayerPrefs.SetInt("sound", soundOn ? 1 : 0);
+    }
+
+    public void OnMusicToggle()
+    {
+        musicOn = MusicToggle.isOn;
+        PlayerPrefs.SetInt("music", musicOn ? 1 : 0);
     }
 
     private void PauseGame()
