2f5e8d799c130ea884c6c910d11c1385a98d47cd
Filip <127895941+Filip002@users.noreply.github.com>
2024-11-23 12:59:55 +0100

    update scoreboardTestScene and ScoreboardController.cs
    add ScoreboardTableText to scoreboardTestScene and set default text

--- Assets/Scripts/ScoreboardController.cs
+++ Assets/Scripts/ScoreboardController.cs
@@ -1,21 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ScoreboardController : MonoBehaviour
 {
     public GameObject scoreboardPanel;
+    public TextMeshProUGUI scoreboardTableText;
+    public int numberOfLevels;
+
+    private string scoreboardTableHeader;
+    private string scoreboardTableContent;
     private void Start()
     {
         scoreboardPanel.SetActive(false);
+        scoreboardTableHeader = "Nr.\t\tWynik\n";
+        scoreboardTableContent = GetScoreboardTableContent();
+        scoreboardTableText.GetComponent<TextMeshProUGUI>().text = scoreboardTableHeader + scoreboardTableContent;
     }
     public void ShowScoreboardPanel()
     {
         scoreboardPanel.SetActive(true);
+
     }
 
     public void CloseScoreboardPanel()
     {
         scoreboardPanel.SetActive(false);
+    }
+
+    private string GetScoreboardTableContent()
+    {
+        string content = string.Empty;
+        for (int i = 0; i<numberOfLevels; i++)
+        {
+            content += (i+1).ToString() + ". .............................................\n";
+        }
+        return content;
     }
 }
