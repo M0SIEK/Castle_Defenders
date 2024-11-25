050c9c509539ea7d6c2f0ad8374ade7b25626588
M0SIEK <mosiek28022003@gmail.com>
2024-11-25 19:30:45 +0100

    ExitGameButton Update

--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -2,6 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 public class MainMenuController : MonoBehaviour
 {
@@ -9,5 +12,16 @@ public class MainMenuController : MonoBehaviour
     {
         // Za³aduj pierwsz¹ scenê z gry
         SceneManager.LoadScene("EndOfRoundScene");
+    }
+
+    public void ExitGame()
+    {
+    #if UNITY_EDITOR
+        // Wy³¹cz tryb Play w edytorze Unity
+        EditorApplication.isPlaying = false;
+    #else
+        // Wy³¹cz aplikacjê w œrodowisku uruchomieniowym
+        Application.Quit();
+    #endif
     }
 }
