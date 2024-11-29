3f92b4ed102bbe12e1d2553e830df1709a7ce8e2
M0SIEK <mosiek28022003@gmail.com>
2024-11-29 15:55:35 +0100

    Add LevelsPanel

---
+++ Assets/Scripts/LevelsPanelController.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelsPanelController : MonoBehaviour
+{
+    public GameObject MainMenu;       // Referencja do g³ównego menu
+    public GameObject LevelsPanel;    // Referencja do panelu wyboru poziomów
+    public CanvasGroup MainMenuCanvasGroup; // CanvasGroup g³ównego menu (do blokowania interakcji)
+
+    public void ReturnToMainMenu()
+    {
+        LevelsPanel.SetActive(false);              // Ukryj panel poziomów
+        MainMenuCanvasGroup.interactable = true;   // Odblokuj interakcje z g³ównym menu
+        MainMenuCanvasGroup.blocksRaycasts = true; // Przywróæ rejestrowanie klikniêæ
+    }
+}
--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -1,4 +1,3 @@
-using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -8,17 +7,20 @@ using UnityEditor;
 
 public class MainMenuController : MonoBehaviour
 {
-    public GameObject MainMenu;
-    public Toggle soundToggle;  // Prze≥πcznik düwiÍku
-    public Toggle musicToggle;  // Prze≥πcznik muzyki
-    public AudioSource musicSource1; // Zmieniono na MusicSource1
+    public GameObject MainMenu;       // G≥Ûwne menu
+    public GameObject LevelsPanel;   // Panel poziomÛw
+    public CanvasGroup MainMenuCanvasGroup; // CanvasGroup g≥Ûwnego menu (do blokowania interakcji)
+    public Toggle soundToggle;       // Prze≥πcznik düwiÍku
+    public Toggle musicToggle;       // Prze≥πcznik muzyki
+    public AudioSource musicSource1; // èrÛd≥o muzyki
 
     private bool soundOn;
     private bool musicOn;
 
     private void Start()
     {
-        MainMenu.SetActive(true);  // Upewnij siÍ, øe menu jest aktywne
+        MainMenu.SetActive(true);  // W≥πcz g≥Ûwne menu
+        LevelsPanel.SetActive(false); // Ukryj panel poziomÛw
 
         // Pobierz ustawienia z PlayerPrefs
         soundOn = PlayerPrefs.GetInt("sound", 1) == 1; // Domyúlnie w≥πczone
@@ -38,6 +40,15 @@ public class MainMenuController : MonoBehaviour
         SceneManager.LoadScene("Demo3Scene");
     }
 
+    public void OpenLevelsPanel()
+    {
+        Debug.Log("OpenLevelsPanel called"); // Sprawdü, czy funkcja dzia≥a
+        LevelsPanel.SetActive(true);               // Pokaø panel poziomÛw
+        Debug.Log("LevelsPanel active after: " + LevelsPanel.activeSelf);
+        MainMenuCanvasGroup.interactable = false;  // Zablokuj interakcje z g≥Ûwnym menu
+        MainMenuCanvasGroup.blocksRaycasts = false; // PrzestaÒ rejestrowaÊ klikniÍcia dla menu
+    }
+
     public void ExitGame()
     {
 #if UNITY_EDITOR
@@ -49,28 +60,26 @@ public class MainMenuController : MonoBehaviour
 #endif
     }
 
-    // Prze≥πcznik düwiÍku - kontroluje globalnπ g≥oúnoúÊ gry
+    // Prze≥πcznik düwiÍku
     public void OnSoundToggle()
     {
         soundOn = soundToggle.isOn;
         PlayerPrefs.SetInt("sound", soundOn ? 1 : 0);
     }
 
-    // Prze≥πcznik muzyki - kontroluje wyciszenie muzyki
+    // Prze≥πcznik muzyki
     public void OnMusicToggle()
     {
         musicOn = musicToggle.isOn;
         PlayerPrefs.SetInt("music", musicOn ? 1 : 0);
-        // Zaktualizuj stan muzyki
         UpdateMusicState();
     }
 
-    // Funkcja do kontrolowania stanu muzyki
     private void UpdateMusicState()
     {
         if (musicSource1 != null)
         {
-            musicSource1.mute = !musicOn; // Wycisz lub w≥πcz MusicSource1
+            musicSource1.mute = !musicOn; // Wycisz lub w≥πcz muzykÍ
         }
     }
 }
